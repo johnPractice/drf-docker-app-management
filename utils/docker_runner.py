1f2162fae10395ea08939be9b6400f2cc00a3510
John <rezamosavian1014@gmail.com>
2022-12-04 12:05:49 +0330

    fix : remove DockerRunner test

--- utils/docker_runner.py
+++ utils/docker_runner.py
@@ -16,8 +16,3 @@ class DockerRunner:
             name=self.docker_object.get_container_name()))
 
 
-d = Docker(image='alpine', envs=['key1:key2', 'key3:key4'],
-           commands=['echo', 'hello', 'world'])
-dr = DockerRunner(d)
-dr.run()
-    
