d72faab675a9cd233ea2fc476afc8c2f75020060
John <rezamosavian1014@gmail.com>
2022-12-04 13:53:55 +0330

    feat : update docker_app model

--- docker_app/models.py
+++ docker_app/models.py
@@ -3,7 +3,6 @@ from utils.randome_name import generate_randome_app_name
 
 
 class ContainerizedApp(models.Model):
-    app_name = models.CharField(
-        max_length=250, default=generate_randome_app_name())
+    container_name = models.CharField(max_length=250, null=False, blank=False)
     image_name = models.CharField(max_length=250, null=False, blank=False)
-    # run_command = models.cha
+    envs = models.CharField(max_length=300, null=True, blank=True)
