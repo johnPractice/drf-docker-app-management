f3236e0ddb24b30a1c7ba8812cff671dbfc2547b
John <rezamosavian1014@gmail.com>
2022-12-04 15:16:18 +0330

    feat : add list container

--- docker_app/routers.py
+++ docker_app/routers.py
@@ -1,18 +1,28 @@
-from rest_framework import generics, mixins, status
+from rest_framework import generics, mixins, status, viewsets
 from rest_framework.views import APIView
 from docker_app.models import ContainerizedApp
-from docker_app.serializers import CreateContainerizedAppSerializer, RetriveContainerizedAppSerializer
+from docker_app.serializers import CreateContainerizedAppSerializer, RetriveContainerizedAppSerializer, RetriveContainerInfo
 from rest_framework.response import Response
 from utils.docker import Docker
 from utils.docker_runner import DockerRunner
 
 
-class DockerContainrizeAppRouter(APIView):
-    def get(self, request, format=None):
+class DockerAppStatusViewset(viewsets.ViewSet):
+    def retrieve(self, reques, pk=None):
         pass
 
-    def post(self, request, format=None):
-        input_serializer = CreateContainerizedAppSerializer(data=request.data)
+    def list(self, request):
+        check_all_container: str = request.query_params.get(
+            'all', 'True').lower()
+        all_container: bool = True if check_all_container == 'true' else False
+        serializer = RetriveContainerInfo(
+            data=DockerRunner.get_all_container_info(all=all_container), many=True)
+        serializer.is_valid(raise_exception=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        input_serializer = CreateContainerizedAppSerializer(
+            data=request.data)
         input_serializer.is_valid(raise_exception=True)
 
         d = Docker(**input_serializer.data)
